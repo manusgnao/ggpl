04ebb5a29757852e47e9ded8254afc6808e54bfa
Manuel Sgnaolin <Manuel Sgnaolin>
2016-10-23 12:11:49 +0200

    parametrizzazione del primo workshop

--- 2016-10-14/workshop_01.py
+++ 2016-10-14/workshop_01.py
@@ -1,15 +1,19 @@
-from larlib import *
+from pyplasm import *
 
-beamsWeight = [4.0, 8.5, 6.0, 4.0, 8.0]
-pilarsHeight = [4.0, 2.5, 5.0]
-pilar_space_tot=0
-
+beamsWeight = [3.0, 3.0, 3.0, 3.0, 3.0, 3.0]
+pilarsHeight = [4.0, 4.0, 3.0]
+bx=1
+bz=1
+px=1
+py=1
+beamDepth = [15.0, 5.0, 3.0, 5.0]
 pilar1 = CUBOID([0,0,0])
 totalArc = STRUCT([pilar1])
 totPilarHeight=0
+t = STRUCT([CUBOID([])])
 
 for j,pilar in enumerate(pilarsHeight):
-	firstPilar = CUBOID([1,1,pilar])
+	firstPilar = CUBOID([px,py,pilar])
 	totBeamWeight=0
 	if j!=0:
 			parzBeam = STRUCT([T(3)(totPilarHeight+1), firstPilar])
@@ -20,7 +24,7 @@ for j,pilar in enumerate(pilarsHeight):
 	totPilarHeight= totPilarHeight + pilar
 
 	for i,beam in enumerate(beamsWeight):
-		parzPilar = CUBOID([1,beam,1])
+		parzPilar = CUBOID([bx,beam,bz])
 		if i==0:
 			if(j==0):
 				nextBeam = STRUCT([T(2)(0.5),T(3)(totPilarHeight), parzPilar])
@@ -32,13 +36,13 @@ for j,pilar in enumerate(pilarsHeight):
 			else:
 				nextBeam = STRUCT([T(2)(totBeamWeight+0.5),T(3)(totPilarHeight+1), parzPilar])
 		totBeamWeight = totBeamWeight + beam
-		nextPilar = CUBOID([1,1,pilar])
+		nextPilar = CUBOID([px,py,pilar])
 		if(j==0):
 			firstPilar = STRUCT([T(2)(totBeamWeight), nextPilar])
 		else:
 			firstPilar = STRUCT([T(2)(totBeamWeight),T(3)(totPilarHeight-pilar+1), nextPilar])
 		arc = STRUCT([firstPilar,nextBeam])
 		totalArc = STRUCT([totalArc,arc])
-
-
-VIEW(totalArc)
+for x,depth in enumerate(beamDepth):
+	t = STRUCT([totalArc, T(1)(depth), t])
+VIEW(t)
