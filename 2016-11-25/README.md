6960b017f3a91c9bc450490fde6fc3711fc63886
Manuel Sgnaolin <Manuel Sgnaolin>
2016-11-26 17:31:39 +0100

    commit of windows and doors

---
+++ 2016-11-25/README.md
@@ -0,0 +1,20 @@
+# WORKSHOP 7
+
+## Descrizione
+Rappresentazione di porta e e finestre
+## Porta1 trovata su web
+![alt text](https://github.com/manusgnao/ggpl/blob/master/2016-11-25/images/porta.jpg "Porta1")
+## Prorta1 riprodotta
+![alt text](https://github.com/manusgnao/ggpl/blob/master/2016-11-25/images/PORTA.png "Riproduzione porta1)")
+## Porta2 trovata su web
+![alt text](https://github.com/manusgnao/ggpl/blob/master/2016-11-25/images/puerta.jpg "Porta2")
+## Prorta2 riprodotta
+![alt text](https://github.com/manusgnao/ggpl/blob/master/2016-11-25/images/PUERTA.png "Riproduzione porta2)")
+## Finestra1 trovata su web
+![alt text](https://github.com/manusgnao/ggpl/blob/master/2016-11-25/images/Window_1.jpg "Finestra1")
+## Finestra1 riprodotta
+![alt text](https://github.com/manusgnao/ggpl/blob/master/2016-11-25/images/WINDOW1.png "Riproduzione finestra1)")
+## Finestra2 trovata su web
+![alt text](https://github.com/manusgnao/ggpl/blob/master/2016-11-25/images/window2.jpg "Finestra2")
+## Finestra2 riprodotta
+![alt text](https://github.com/manusgnao/ggpl/blob/master/2016-11-25/images/WINDOW2.png "Riproduzione finestra2)")
---
+++ 2016-11-25/workshop_07.py
@@ -0,0 +1,67 @@
+""" workshop 07 """
+
+from pyplasm import *
+ """ xArray e' l'array corrispondende all'asse x
+ 	 yArray e' l'array corrispondende all'asse yArray
+ 	 occupancy e' l'array di array che stabilisce quale celle sono di vetro e quali di legno """
+#window1
+xArray = [0.0,0.1,0.5,0.6,1.0,1.1]
+yArray = [0.0,0.1,1.0,1.1,1.3,1.4]
+occupancy = [[1,1,1,1,1],[1,0,1,0,1],[1,1,1,1,1],[1,0,0,0,1],[1,1,1,1,1]]
+
+#window2
+#xArray = [0.0,0.1,0.5,0.6,1.0,1.1]
+#yArray = [0.0,0.1,0.6,0.7,1.2,1.3]
+#occupancy = [[1,1,1,1,1],[1,0,1,0,1],[1,1,1,1,1],[1,0,1,0,1],[1,1,1,1,1]]
+
+#puerta
+#xArray = [0.0,0.2,0.5,0.7]
+#yArray = [0.0,0.2,0.4,0.5,1.6,1.8]
+#occupancy = [[1,1,1],[1,0,1],[1,1,1],[1,0,1],[1,1,1]]
+
+#porta
+#xArray = [0.0,0.2,0.4,0.6,0.8,1.0]
+#yArray = [0.0,0.2,0.8,1.0,1.6,1.8]
+#occupancy = [[1,1,1,1,1],[1,0,1,0,1],[1,1,1,1,1],[1,0,1,0,1],[1,1,1,1,1]]
+
+""" struttura iniziale"""
+zero = QUOTE([.0,.0])
+initStruct = STRUCT([zero])
+""" funzione che crea la finestra: i vetri sono blu i legni sono bianchi """
+def window(i, j, partStruct):
+	if j != len(yArray)-1:
+		if  i != len(xArray)-1:
+			if occupancy[j][i]==0:
+				x = COLOR(BLUE)(CUBOID([xArray[i+1]-xArray[i],yArray[j+1]-yArray[j],0.01]))
+				struct = STRUCT([T([1,2,3])([xArray[i],yArray[j],0.1/3]), x])
+			else:
+				x = COLOR(WHITE)(CUBOID([xArray[i+1]-xArray[i],yArray[j+1]-yArray[j],0.1]))
+				struct = STRUCT([T([1,2])([xArray[i],yArray[j]]), x])
+			partStruct = STRUCT([partStruct,struct])
+			return window(i+1, j, partStruct)
+		else:
+			return window(0, j+1, partStruct)
+	else:
+		VIEW(partStruct)
+
+""" funzione che crea la porta """
+
+def door(i, j, partStruct):
+	if j != len(yArray)-1:
+		if  i != len(xArray)-1:
+			if occupancy[j][i]==0:
+				x = COLOR(WHITE)(CUBOID([xArray[i+1]-xArray[i],yArray[j+1]-yArray[j],0.01]))
+				struct = STRUCT([T([1,2,3])([xArray[i],yArray[j],0.1/3]), x])
+			else:
+				x = COLOR(BROWN)(CUBOID([xArray[i+1]-xArray[i],yArray[j+1]-yArray[j],0.1]))
+				struct = STRUCT([T([1,2])([xArray[i],yArray[j]]), x])
+			partStruct = STRUCT([partStruct,struct])
+			return door(i+1, j, partStruct)
+		else:
+			return door(0, j+1, partStruct)
+	else:
+		VIEW(partStruct)
+
+
+window(0, 0, initStruct)
+#door(0, 0, initStruct)
